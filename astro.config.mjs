ba52dab82e0c7a7ca46fc6a946103c0ba1c87501
Bart Krystman <bart@routedigit.com>
2026-01-18 09:14:40 -0700

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 import { defineConfig } from "astro/config";
 
+const repo = process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "";
+const isUserPages = repo.endsWith(".github.io");
+const base = isUserPages ? "/" : `/${repo}`;
+
 export default defineConfig({
-  output: "static"
+  output: "static",
+  base
 });
